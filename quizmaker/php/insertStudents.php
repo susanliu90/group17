2b9c395a218debb090b0d31648ea2756820ea3e8
jec047 <jjchang1223@gmail.com>
2013-09-06 13:53:00 -0700

    insert students is in progress

--- quizmaker/php/insertStudents.php
+++ quizmaker/php/insertStudents.php
@@ -1,74 +1,117 @@
-
 <?php
-	// set up the SQL connection
-	$db_hostname = "hostname";
+	include 'functions.php';
+	// SET UP THE SQL CONNECTION
+	$db_hostname = "localhost";
 	$db_database = "quizzler"; 
-	$db_username = "user";
-	$db_password = "";
+	$db_username = "s17";
+	$db_password = "groupe17";
 
-	// connect to the DB
+	// CONNECT TO THE DATABASE
 	$db_server = mysql_connect($db_hostname, $db_username, $db_password)
-		or die("There was an error connecting to the database:" .mysql_error());
+		or die("There was an error connecting to the database:" . mysql_error());
 
-	// select the DB
-		mysql_select_db($db_database)
+	// SELECT THE DATABASE
+	mysql_select_db($db_database)
 	    or die("Unable to select database: " . mysql_error());
-		
-	$firstName   = $_POST["stu-fname"];
-	$lastName    = $_POST["stu-lname"];
-	$email	     = $_POST["stu-email"];
-	$courseNum   = $_POST["course"];
+
+	// GRAB THE POST INFORMATION
+	$lastName	     = $_POST["stu-lname"];
+	$firstName  = $_POST["stu-fname"];
+	$email 			 =$_POST["stu-email"];
+	$course 			 =$_POST["stu-course"];
 	
-	// see if student already exist in users
-	$sql="SELECT * FROM users
-		WHERE firstName = $firstName
-		AND lastName = $lastName
-		AND email = $email
-		AND level='student'"
-	$result = mysql_query(sql);
-
-	// if student does not exit in users, insert into all tables
-	if (!$result) {
-		// insert data into users
-		$sql="INSERT INTO users (lastName, firstName, email, level
-			VALUES ($lastName, $firstName, $email, 'student'"
-		$result = mysql_query($sql);
-	}
-	else {
-		// find user_ids from users
-		$sql="SELECT user_id FROM users
-			WHERE firstName = $firstName
-			AND lastName = $lastName
-			AND email = $email
-			AND level='student'"
-		$result = mysql_query($sql);
-		// insert user_ids into student - WILSON
-	}
-
-	// we should also check if the student has already been previously added to the same class - WILSON
-
-	// insert data into student (grades_id is NULL)
-	$sql="INSERT INTO students (lastName, firstName, email, course_num)
-		VALUES ($lastName, $firstName, $email, $courseNum)"; 
+	//echo "$course_num";
+	
+	// CONSTRUCT THE SQL QUERY TO CHECK EXISTENCE OF TA
+	$sql = 
+	"
+	SELECT email FROM users
+	WHERE email = '$email';
+	";
 	$result = mysql_query($sql);
-
-	// find student_ids to put into student_list
-	$sql="SELECT student_id FROM students 
-		WHERE firstName = $firstName
-		AND lastName = $lastName
-		AND email = $email
-		AND course_num = $courseNum"
-	$result = mysql_query($sql);
-	$studentId = mysql_num_rows($result);
-	$sql="INSERT INTO student_list (student_id, course_num)
-		VALUES ($studentId, $courseNum)";
-	if($result){
-		header("Location: ../html/addstudents.html");
+	$data = mysql_fetch_array($result);
+	
+	
+	$ID = 
+	"
+	SELECT user_id FROM users
+	WHERE email = '$email';
+	";
+	
+	
+	$result1 = mysql_query($ID);
+	while ($data1 = mysql_fetch_assoc($result1))
+	{
+		//echo $data1["user_id"];
+		$user_id = $data1["user_id"];
 	}
-	else {
-		echo "ERROR";
-	}
 
-	//close mySQL
+	$dup_ta =
+	"
+	SELECT ta_id FROM tas
+	WHERE user_id = '$user_id';
+	";
+	$result2 = mysql_query($dup_ta);
+	while ($data2 = mysql_fetch_assoc($result2))
+	{
+		//echo $data1["user_id"];
+		$ta_id = $data2["ta_id"];
+	}
+	
+	$prof =
+	"
+		SELECT prof_id FROM professors
+		WHERE user_id = '$user_id';
+	";
+	result3 = mysql_query($prof);
+	while ($data3 = mysql_fetch_assoc($result3))
+	{
+		$prof_id = $data3["prof_id"];
+	}
+	
+	echo "$prof_id";
+	
+	//if there is no duplicate. if the user_id is blank.
+	//we are inserting user_id but we dont prompt the user for user_id
+	if("".$data['email'] == $email)
+	{	
+		//echo "hi<br>";
+		//echo $course_num;
+		if ( exc($course_num) )
+		{
+			//echo "course num : $course_num  <br>";
+			if( exp($prof_id) )
+			{
+				//echo "got here 1";
+				// CONSTRUCT THE SQL QUERY TO INSERT NEW TA
+				$sql = 
+				"
+				INSERT INTO tas (
+					user_id,
+					course_num
+				) 
+				VALUES (
+					'$user_id', 
+					'$course_num'
+				);
+				"; 	
+				$result = mysql_query($sql);	
+			}	
+		}		
+	}
+	else //this is if there is a duplicate
+	{
+		echo 
+			"
+			<script type='text/javascript'>
+				alert('User does not exist. Please create become a User first.');
+			</script>
+			";
+
+	}
+	// REDIRECT USER TO SAME PAGE AFTER SUBMISSION
+	header("Location: ../html/addtas.html");
+	
+	// CLOSE THE SQL CONNECTION
 	mysql_close();
 ?>
